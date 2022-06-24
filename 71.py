169d374c658a07a3911d0b469ee8d98ed8ec0c58
AlexeiChizh <achizhov199@yandex.ru>
2022-06-24 18:58:51 +0300

    first commit

---
+++ 71.py
@@ -0,0 +1,37 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+page = requests.get('http://mfd.ru/currency/?currency=USD')
+soup = BeautifulSoup(page.text, 'html.parser')
+table = soup.find('table',{'class':'mfd-table mfd-currency-table'})
+
+rows = table.find_all('td')
+rows = (i.text for i in rows)
+
+e=[]
+for i in rows:
+	e += str(i).split()
+
+x=[]
+y=[]
+
+for i in range(len(e) - 1, 3, -4):
+    x.append(str(e[i - 2]))
+    y.append(float(str(e[i - 1])))
+
+figure, ax = plt.subplots()
+
+ax.plot(x, y)
+ax.set_title('Курс доллара к рублю', fontsize = 15, color = 'blue')
+ax.set_xlabel('Дата', fontsize = 12, color = 'blue')
+ax.set_ylabel('Курс', fontsize = 12, color = 'blue')
+
+ax.xaxis.set_major_locator(MaxNLocator(6))
+
+ax.grid(True)
+
+plt.show()
